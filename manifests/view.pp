59021dda30786d21e26de8363cc76f683df419a5
Justin Cattle <j@ocado.com>
2019-09-19 15:18:50 +0100

    Added support for per view forwarders - can be set to empty list for null forwarders

--- manifests/view.pp
+++ manifests/view.pp
@@ -5,6 +5,7 @@ define bind::view (
     $match_destinations           = '',
     $zones                        = [],
     $dynamic_dbs                  = [],
+    $forwarders                   = undef,
     $recursion                    = true,
     $recursion_match_clients      = 'any',
     $recursion_match_destinations = '',
